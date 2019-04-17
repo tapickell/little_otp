fff80e1c3f421d3f0e48c656f6f0c9018f2f4f7c
tpickle <tapickell@gmail.com>
2019-04-17 12:10:23 -0500

    finish pool server logic

--- chapt6/pooly/lib/pooly/server.ex
+++ chapt6/pooly/lib/pooly/server.ex
@@ -2,44 +2,69 @@ defmodule Pooly.Server do
   use GenServer
 
   defmodule State do
-    defstruct sup: nil, size: nil, mfa: nil, worker_sup: nil, workers: nil
+    defstruct sup: nil, size: nil, worker_sup: nil, workers: nil, monitors: nil
   end
 
-  def start_link(sup, pool_config) do
-    GenServer.start_link(__MODULE__, [sup, pool_config], name: __MODULE__)
+  def start_link(options) do
+    GenServer.start_link(__MODULE__, options, name: __MODULE__)
   end
 
-  def init([sup, pool_config]) when is_pid(sup) do
-    init(pool_config, %State{sup: sup})
+  def checkin(w_pid) do
+    GenServer.call(__MODULE__, {:checkin, w_pid})
   end
 
-  def init([{:mfa, mfa} | rest], state) do
-    init(rest, %{state | mfa: mfa})
+  def checkout() do
+    GenServer.call(__MODULE__, :checkout)
   end
 
-  def init([{:size, size} | rest], state) do
-    init(rest, %{state | size: size})
+  def status() do
+    GenServer.call(__MODULE__, :status)
   end
 
-  def init([_ | rest], state) do
-    init(rest, state)
-  end
-
-  def init([], state) do
+  def init([{:sup, sup}, {:size, size}]) when is_pid(sup) and is_integer(size) do
+    monitors = :ets.new(:monitors, [:private])
     send(self(), :start_worker_supervisor)
-    {:ok, state}
+    {:ok, %State{sup: sup, size: size, monitors: monitors}}
   end
 
-  def handle_info(:start_worker_supervisor, state = %{sup: sup, mfa: _mfa, size: size}) do
-    {:ok, w_sup} = Supervisor.start_child(sup, Pooly.WorkerSupervisor.child_spec([restart: :temporary]) )
+  def handle_call({:checkin, w_pid}, %{workers: workers, monitors: monitors} = state) do
+    case :ets.lookup(monitors, w_pi) do
+      [{pid, ref}] ->
+        with true <- Process.demonitor(ref),
+             true <- :ets.delete(monitors, pid) do
+          {:noreply, %{state | workers: [pid | workers]}}
+        end
+      [] ->
+        {:noreply, state}
+    end
+  end
 
-    workers = 1..size
-    |> Enum.map(fn _ ->
-      {:ok, pid} = Pooly.WorkerSupervisor.start_child([])
-      pid
-    end)
+  def handle_call(:checkout, {from_pid, _ref}, %{workers: workers, monitors: monitors} = state) do
+    case workers do
+      [worker | rest] ->
+        ref = Process.monitor(from_pid)
+        true = :ets.insert(monitors, {worker, ref})
+        {:reply, worker, %{state | workers: rest}}
+      [] ->
+        {:reply, :noproc, state}
+    end
+  end
+
+  def handle_call(:status, _from, %{workers: workers, monitors: monitors} = state) do
+    {:reply, {length(workers), :ets.info(monitors, :size)}, state}
+  end
+
+  def handle_info(:start_worker_supervisor, %{sup: sup, size: size} = state) do
+    {:ok, w_sup} =
+      Supervisor.start_child(sup, Pooly.WorkerSupervisor.child_spec(restart: :temporary))
+
+    workers =
+      1..size
+      |> Enum.map(fn _ ->
+        {:ok, pid} = Pooly.WorkerSupervisor.start_child([])
+        pid
+      end)
 
     {:noreply, %{state | worker_sup: w_sup, workers: workers}}
   end
-
 end
